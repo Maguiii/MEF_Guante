835932265fab14b1abf7b48092f80529f51073cd
Maguiii <magaliarebull@gmail.com>
2023-06-29 10:02:53 -0300

    Cuenta cant de pulsaciones y manda los datos

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,6 @@
 #include <Arduino.h>
 /*
-   Arreglar este programa que esta horrible
-
-   La parte de contar anularaciones esta mal asi no funciona el bluetooth
-
-   (Solo envia mensajes si se esta anularando, no avisa si se deja de anularar)
-
-   FUNCIONA CON EL PROGRAMA MEF_SuperGuanti
-
-   Los pines ya estan declarados para usar con la plaqueta
+  No esta probado que funcione la cuenta ni el envio de datos sobre las pulsaciones de cada dedo
 */
 #define indice  A0
 #define mayor   A1 
@@ -43,108 +35,126 @@ void setup() {
 
 void loop() {
 
-   if(digitalRead(indice)== HIGH){
-      Serial.write('derecha');
-      delay(100);
+   switch (estadoPrograma)
+   {
+      case 0:
+         if(digitalRead(indice) == HIGH || digitalRead(mayor) == HIGH || digitalRead(anular) == HIGH || digitalRead(menique) == HIGH || digitalRead(pulgar) == HIGH){
+            estadoPrograma = 0;
+         }
+         else{
+            estadoPrograma = 1;
+         }
 
-      switch (estadoIndice){
-         case 0:
-            if(digitalRead(indice) == HIGH){
-               estadoIndice = 1;
-            }
-         break;
-         case 1:
-            if(digitalRead(indice) == LOW){
-               contIndice++;
-               estadoIndice = 0;
-            }
-         break;
-      }
-   }
+         if(digitalRead(indice)== HIGH){
+            Serial.println("derecha");
+            delay(100);
 
-
-   if(digitalRead(mayor)== HIGH){
-      Serial.write('izquierda');
-      delay(100);
-
-      switch (estadoMayor)
-      {
-         case 0:
-            if(digitalRead(mayor) == HIGH){
-               estadoMayor = 1;
+            switch (estadoIndice){
+               case 0:
+                  if(digitalRead(indice) == HIGH){
+                     estadoIndice = 1;
+                  }
+               break;
+               case 1:
+                  if(digitalRead(indice) == LOW){
+                     contIndice++;
+                     estadoIndice = 0;
+                  }
+               break;
             }
-         break;
-         case 1:
-            if(digitalRead(mayor) == LOW){
-               contMayor++;
-               estadoMayor = 0;
-            }
-         break;
-      }
-   }
+         }
+         if(digitalRead(mayor)== HIGH){
+            Serial.println("izquierda");
+            delay(100);
 
+            switch (estadoMayor)
+            {
+               case 0:
+                  if(digitalRead(mayor) == HIGH){
+                     estadoMayor = 1;
+                  }
+               break;
+               case 1:
+                  if(digitalRead(mayor) == LOW){
+                     contMayor++;
+                     estadoMayor = 0;
+                  }
+               break;
+            }
+         }
+         if(digitalRead(anular)== HIGH){
+            Serial.println("adelante");
+            delay(100);
 
-   if(digitalRead(anular)== HIGH){
-      Serial.write('adelante');
-      delay(100);
+            switch (estadoAnular)
+            {
+               case 0:
+                  if(digitalRead(anular) == HIGH){
+                     estadoAnular = 1;
+                  }
+               break;
+               case 1:
+                  if(digitalRead(anular) == LOW){
+                     contAnular++;
+                     estadoAnular = 0;
+                  }
+               break;
+            }
+         }
+         if(digitalRead(menique)== HIGH){
+            Serial.println("atras");
+            delay(100);
 
-      switch (estadoAnular)
-      {
-         case 0:
-            if(digitalRead(anular) == HIGH){
-               estadoAnular = 1;
+            switch (estadoMenique)
+            {
+               case 0:
+                  if(digitalRead(menique) == HIGH){
+                     estadoMenique = 1;
+                  }
+               break;
+               case 1:
+                  if(digitalRead(menique) == LOW){
+                     contMenique++;
+                     estadoMenique = 0;
+                  }
+               break;
             }
-         break;
-         case 1:
-            if(digitalRead(anular) == LOW){
-               contAnular++;
-               estadoAnular = 0;
-            }
-         break;
-      }
-   }
-   if(digitalRead(menique)== HIGH){
-      Serial.write('7');
-      delay(100);
+         }         
+         if(digitalRead(pulgar)== HIGH){
+            Serial.println("abajo");
+            delay(100);
 
-      switch (estadoMenique)
-      {
-         case 0:
-            if(digitalRead(menique) == HIGH){
-               estadoMenique = 1;
+            switch (estadoPulgar)
+            {
+               case 0:
+                  if(digitalRead(pulgar) == HIGH){
+                     estadoPulgar = 1;
+                  }
+               break;
+               case 1:
+                  if(digitalRead(pulgar) == LOW){
+                     contPulgar++;
+                     estadoPulgar = 0;
+                  }
+               break;
             }
-         break;
-         case 1:
-            if(digitalRead(menique) == LOW){
-               contMenique++;
-               estadoMenique = 0;
-            }
-         break;
-      }
+         }
+      break;
+      case 1:
+         String cadenaIndice = String(contIndice);
+         String cadenaMayor= String(contMayor);
+         String cadenaAnular = String(contAnular);
+         String cadenaMenique = String(contMenique);
+         String cadenaPulgar = String(contPulgar);
 
-   }
-   if(digitalRead(pulgar)== HIGH){
-      Serial.write('9');
-      delay(100);
+         Serial.print(contIndice);
+         Serial.print(contMayor);
+         Serial.print(contAnular);
+         Serial.print(contMenique);
+         Serial.print(contPulgar);
 
-      switch (estadoPulgar)
-      {
-         case 0:
-            if(digitalRead(pulgar) == HIGH){
-               estadoPulgar = 1;
-            }
-         break;
-         case 1:
-            if(digitalRead(pulgar) == LOW){
-               contPulgar++;
-               estadoPulgar = 0;
-            }
-         break;
-      }
+         if(digitalRead(indice) == HIGH || digitalRead(mayor) == HIGH || digitalRead(anular) == HIGH || digitalRead(menique) == HIGH || digitalRead(pulgar) == HIGH)
+            estadoPrograma = 0;
+      break;
    }
 }
-
-
-
-// if(no hay nada anularado)
-// mandar datos de contador
